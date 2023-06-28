8cbf0e1b68c467a9ca89a5f0efbf7d4159169ed3
JorgeLMF <63150944+JorgeLMF@users.noreply.github.com>
2023-06-28 18:20:18 -0500

    modified home view

--- lib/recruitment/ui/home/home_view.dart
+++ lib/recruitment/ui/home/home_view.dart
@@ -25,38 +25,57 @@ class _HomeViewState extends State<HomeView> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SafeArea(
-          child: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.all(20.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'Recruitech',
-                  style: TextStyle(
-                    fontSize: 32.0,
-                    fontFamily: 'Gilroy',
-                    fontWeight: FontWeight.w700,
-                    color: Color(0xFF0C1E38),
+      appBar: AppBar(
+        title: SafeArea(
+            child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Recruitech',
+                    style: TextStyle(
+                      fontSize: 32.0,
+                      fontFamily: 'Gilroy',
+                      fontWeight: FontWeight.w700,
+                      color: Color(0xFF0C1E38),
+                    ),
                   ),
-                ),
-                GestureDetector(
-                  onTap: () => Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) =>
-                          DeveloperProfileView(developer: widget.developer))),
-                  child: CircleAvatar(
-                    radius: 28.0,
-                    backgroundImage:
-                        NetworkImage(widget.developer.profilePicture),
+                  GestureDetector(
+                    onTap: () => Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) =>
+                            DeveloperProfileView(developer: widget.developer))),
+                    child: CircleAvatar(
+                      radius: 28.0,
+                      backgroundImage:
+                          NetworkImage(widget.developer.profilePicture),
+                    ),
                   ),
-                )
-              ],
+                ],
+              ),
             ),
-          )
+          ],
+        )),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(8),
+        children: const <Widget>[
+          Text('FeaturedJob'),
+          Column(
+            children: [
+              Text('AcmeCompany'),
+            ],
+          ),
+          Text('Recommend For you'),
+          Column(
+            children: [
+              Text('AcmeCompany'),
+            ],
+          ),
         ],
-      )),
+      ),
     );
   }
 }
